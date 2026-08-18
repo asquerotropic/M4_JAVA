12a43296da93b949fc9bbe186f9590317d07e3bc
Eric CH <erikc.77@gmail.com>
2026-08-18 15:37:56 +0000

    Sesion06 20260818

---
+++ S06/Celular.java
@@ -0,0 +1,21 @@
+package S06;
+
+public class Celular extends Dispositivo{
+	private String sistemaOp;
+	private int almacenamiento;
+
+	public Celular(String marca, String modelo, String sistemaOp, int almacenamiento) {
+		super(marca, modelo);
+		this.sistemaOp = sistemaOp;
+		this.almacenamiento = almacenamiento;
+	}
+
+	void mostrarDetalles() {
+		mostrarInfo();
+		System.out.println("Sistema Operativo: " + sistemaOp + "\nAlmacenamiento: " + almacenamiento + "GB.");
+	}
+
+	void tomarFoto() {
+		System.out.println("El " + marca + " " + modelo + " ha tomado una foto.");
+	}
+}
---
+++ S06/Dispositivo.java
@@ -0,0 +1,19 @@
+package S06;
+
+public class Dispositivo {
+	protected String marca;
+	protected String modelo;
+
+	public Dispositivo(String marca, String modelo) {
+		this.marca = marca;
+		this.modelo = modelo;
+	}
+
+	void encender() {
+		System.out.println("El dispositivo" +  marca +  " " + modelo + " se ha encendido.");
+	}
+
+	void mostrarInfo() {
+		System.out.println("Marca: " +  marca +  "\nModelo: " + modelo);
+	}
+}
