e0ce8917164f07e97a8800be414086e9fb1754f2
Erikc <erikc.77@gmail.com>
2026-09-17 02:03:02 -0600

    Practica v2

--- ACT4B/main/Main.java
+++ ACT4B/main/Main.java
@@ -17,10 +17,7 @@ public class Main {
 		System.out.println("\n-------------------- Ventanilla de Trámites Ciudadanos --------------------");
 		while (!salir) {
 			System.out.println("\n-------------------- Menú Principal\n");
-			System.out.println("1. Registrar nuevo trámite de licencia.");
-			System.out.println("2. Consultar historial de trámites.");
-			System.out.println("3. Salir del sistema.");
-			System.out.print("Elija una opción: ");
+			System.out.print("1. Registrar nuevo trámite de licencia.\n2. Consultar historial de trámites.\n3. Salir del sistema.\nElija una opción: ");
 			int op = sc.nextInt();
 			sc.nextLine();
 
@@ -36,11 +33,22 @@ public class Main {
 				Ciudadano ciudadano = new Ciudadano(nombre, curp, telefono);
 
 				System.out.println("\n[B] Datos del trámite");
-				System.out.print("-> Tipo de licencia: Primera vez / Renovación: ");
-				String tipoLicencia = sc.nextLine();
+				System.out.print("-> Tipo de licencia:\n	1. Primera vez.\n	2. Renovación.\nElija una opción: ");
+				int opLicencia = sc.nextInt();
+				sc.nextLine();
+				String tipoLicencia = "";
+
+				if (opLicencia == 1) {
+					tipoLicencia = "PRIMERA VEZ.";
+				} else if (opLicencia == 2) {
+					tipoLicencia = "RENOVACIÓN.";
+				} else {
+					tipoLicencia = "NO ESPECIFICADO.";
+					System.out.println("\n¡Advertencia! Opción no válida, trámite marcado como no especificado.");
+				}
 
 				Requisito requisito = new Requisito("Identificación oficial y comprobante de domicilio");
-				System.out.print("-> ¿El ciudadano entregó todos los documentos? 1. Si - 2. No: ");
+				System.out.print("\n-> ¿El ciudadano entregó todos los documentos? 1. Si - 2. No: ");
 				int entrego = sc.nextInt();
 
 				if (entrego == 1) {
@@ -57,13 +65,13 @@ public class Main {
 					System.err.println("\n -> Aún no hay ningún trámite registrado en el sistema.");
 				} else {
 					for (int i = 0; i < listaSolicitudes.size(); i++) {
-						System.out.println("\n	>> Registro #" + (i + 1) + ": ");
+						System.out.println("\n	>> Registro #" + (i + 1) + " <<");
 						listaSolicitudes.get(i).consultarEstado();
 					}
 				}
 			} else if (op == 3) {
 				salir = true;
-				System.out.println("\n	-> Cerrando la ventanilla... ¡Se han guardado " + listaSolicitudes.size() + " trámites hoy!\n");
+				System.out.println("\n-> Cerrando la ventanilla... ¡Se han guardado " + listaSolicitudes.size() + " trámites hoy!\n");
 			} else {
 				System.out.println("\n-> Opción no válida. Por favor intenta de nuevo.");
 			}
